0d825620abd678497df847cb9575340eceae29c0
Suguru Murakami <sugumura@gmail.com>
2015-11-10 19:00:37 +0900

    modify save screenshot native photo app

--- src/ios/CDVScreenshot.m
+++ src/ios/CDVScreenshot.m
@@ -1,15 +1,41 @@
 #import <Cordova/CDV.h>
 #import "CDVScreenshot.h"
 
-@implementation CDVScreenshot
+
+@implementation CDVScreenshot {
+    CDVInvokedUrlCommand *commnd;
+}
 
 - (void) now:(CDVInvokedUrlCommand*)command
 {
-    options = [command.arguments objectAtIndex:0];
-    callbackId = command.callbackId;
+    self->commnd = command;
+    UIImage *image = [self getScreenshot];
 
-    // TODO: implementation
-    NSLog("Call now");
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(onCompleteCapture:didFinishSavingWithError:contextInfo:), nil);
+}
+
+
+- (void)onCompleteCapture:(UIImage *)screenImage didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    if (error) {
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:[self->commnd callbackId]];
+    } else {
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:[self->commnd callbackId]];
+    }
+}
+
+
+- (UIImage *)getScreenshot
+{
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    CGRect rect = [keyWindow bounds];
+    UIGraphicsBeginImageContextWithOptions(rect.size, YES, 0);
+    [keyWindow drawViewHierarchyInRect:keyWindow.bounds afterScreenUpdates:YES];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
 }
 
 @end
